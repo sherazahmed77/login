fc5434a3c43281845e37fd320f8b0f1630369e5e
sherazahmed77 <sherazahmed5177@gmail.com>
2024-01-25 15:03:12 +0500

    completed signup page

--- src/config.js
+++ src/config.js
@@ -1,2 +1,23 @@
 const mangose=require('mangose');
-const connect=mongose.connect("");
+const connect=mongose.connect("momgodb://localhost:28018/Login");
+
+
+connect.then(()=>{
+    console.log("Database Connected Successfully!");
+})
+.catch(()=>{
+    console.log("Database Cannot be Connected");
+});
+
+const LoginSchema=new mongose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+const collection=new mongose.model("users", LoginSchema);
+module.exports=collection;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 const express=require('express');
 const path=require('path');
 const bcrypt=require('bcrypt');
+const  collection=require("./config");
 
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
 app.use(express.static("public"));
 
 app.set('view engine','ejs');
 
 app.get("/", (req,res) =>{
 res.render("login");
-})
+});
 
 app.get("/signup", (req,res)=> {
     res.render("signup");
-})
+});
+app.post("/Signup", async (req,res)=>{
+    const data={
+        name: req.body.username,
+        password: req.body.password
+    }
+    const existinguser= await collection.findOne({name: data.name});
+    if(existinguser){
+        res.send("Username already exixts. Please try another.");
+    }
+    else{
+        const userdata= await collection.insertMany(data);
+        console.log(userdata);
+    }
+});
+
 
 const port = 5000;
 app.listen(port, () => {
